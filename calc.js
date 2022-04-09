a9df4bad0e3356c71956b4589c57890390391157
Kegan Leatherow <leatherowke@gmail.com>
2022-04-09 15:26:43 -0500

    initial commit

---
+++ calc.js
@@ -0,0 +1,28 @@
+const { Console } = require('console');
+const readline = require('readline');
+const { CLIENT_RENEG_LIMIT } = require('tls');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+
+    Console.log("mathsymbol:", mathSymbol)
+    console.log("num1:", num1)
+    console.log("nums2:", num2)
+
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
