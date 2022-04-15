642074989db148e56559428d77a9e9d86d81f4a8
Kegan Leatherow <leatherowke@gmail.com>
2022-04-14 21:23:32 -0500

    arrays.js

--- arrays.js
+++ arrays.js
@@ -21,7 +21,8 @@ console.log(faveColors)
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+let middleNums = numbers.slice(1,4)
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -36,3 +37,10 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+const answers =[] ;
+for (i = 0; i < bigOrSmallArray.length; i++){
+ if(bigOrSmallArray[i]> 100){
+   answers.push('greater')
+ } 
+}
+ console.log(answers)
